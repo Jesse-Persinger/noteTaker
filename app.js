f260c1e614017749b89dd974e3aa2f9e1492cccf
Jesse persinger <70872264+Mau5trap7791@users.noreply.github.com>
2021-01-14 23:11:56 -0500

    24

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require ("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 const fs = require('fs');
 const { createConnection } = require("net");
 const notes = fs.readFileSync('db/db.json');
